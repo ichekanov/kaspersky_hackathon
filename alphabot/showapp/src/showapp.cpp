040d1515a489d6d6da76317e9e4d238bceb8eac4
Ivan Chekanov <is.2003.01@gmail.com>
2022-11-19 17:05:55 +0300

    Implemented IPC

--- alphabot/showapp/src/showapp.cpp
+++ alphabot/showapp/src/showapp.cpp
@@ -4,25 +4,25 @@
 #include <stdint.h>
 #include <coresrv/nk/transport-kos.h>
 #include <coresrv/sl/sl_api.h>
-#include <alphabot/Showapp.edl.h>
+#include <mosquitto/Showapp.edl.h>
 
-static nk_err_t Printnum_impl(struct alphabot_Printnum *self,
-                          const struct alphabot_Printnum_Printnum_req *req,
+static nk_err_t Printnum_impl(struct mosquitto_Printnum *self,
+                          const struct mosquitto_Printnum_Printnum_req *req,
                           const struct nk_arena *req_arena,
-                          struct alphabot_Printnum_Printnum_res *res,
+                          struct mosquitto_Printnum_Printnum_res *res,
                           struct nk_arena *res_arena)
 {
-    fprintf(stderr,"[ShowApp] Recieved number: %d\n", req->value);
+    fprintf(stderr,"[ShowApp] Recieved number: %d, %d, %d\n", req->value1, req->value2, req->value3);
     return NK_EOK;
 }
 
-static struct alphabot_Printnum *CreatePrintnumImpl()
+static struct mosquitto_Printnum *CreatePrintnumImpl()
 {
-    static const struct alphabot_Printnum_ops ops = {
+    static const struct mosquitto_Printnum_ops ops = {
         .Printnum = Printnum_impl
     };
 
-    static struct alphabot_Printnum impl = {
+    static struct mosquitto_Printnum impl = {
         .ops = &ops
     };
 
@@ -38,22 +38,22 @@ int main(void)
 
     NkKosTransport_Init(&transport, handle, NK_NULL, 0);
 
-    alphabot_Showapp_entity_req req;
-    char req_buffer[alphabot_Showapp_entity_req_arena_size];
+    mosquitto_Showapp_entity_req req;
+    char req_buffer[mosquitto_Showapp_entity_req_arena_size];
     struct nk_arena req_arena = NK_ARENA_INITIALIZER(req_buffer,
                                         req_buffer + sizeof(req_buffer));
 
-    alphabot_Showapp_entity_res res;
-    char res_buffer[alphabot_Showapp_entity_res_arena_size];
+    mosquitto_Showapp_entity_res res;
+    char res_buffer[mosquitto_Showapp_entity_res_arena_size];
     struct nk_arena res_arena = NK_ARENA_INITIALIZER(res_buffer,
                                         res_buffer + sizeof(res_buffer));
 
-    alphabot_Printnum_component component;
+    mosquitto_Printnum_component component;
 
-    alphabot_Printnum_component_init(&component, CreatePrintnumImpl());
+    mosquitto_Printnum_component_init(&component, CreatePrintnumImpl());
 
-    alphabot_Showapp_entity entity;
-    alphabot_Showapp_entity_init(&entity, &component);
+    mosquitto_Showapp_entity entity;
+    mosquitto_Showapp_entity_init(&entity, &component);
 
     fprintf(stderr, "Hello I'm showapp application. I'm ready\n");
 
@@ -69,7 +69,7 @@ int main(void)
                               &req_arena) != NK_EOK) {
             fprintf(stderr, "nk_transport_recv error\n");
         } else {
-            alphabot_Showapp_entity_dispatch(&entity, &req.base_, &req_arena,
+            mosquitto_Showapp_entity_dispatch(&entity, &req.base_, &req_arena,
                                         &res.base_, &res_arena);
         }
         if (nk_transport_reply(&transport.base,
--- alphabot/subscriber/src/subscriber.cpp
+++ alphabot/subscriber/src/subscriber.cpp
@@ -1,18 +1,88 @@
 #include <iostream>
 #include <string_view>
-
+#include <string>
 #include "general.h"
 #include "subscriber.h"
 #include <coresrv/nk/transport-kos.h>
 #include <coresrv/sl/sl_api.h>
-#include <alphabot/Printnum.idl.h>
+#include <mosquitto/Printnum.idl.h>
 
 using namespace std::literals;
 
 constexpr auto Topic = "my/awesome/topic"sv;
 
+#include <iostream>
+#include <fstream>
+
+#include "json.hpp"
+#include <list>
+
+using namespace std;
+using json = nlohmann::json;
+
+tuple<int, int, int> cmdAuto(const json &cmd){
+    cout << "markers_floor" << endl;
+    for (int i = 0;i < cmd["markers_screen"].size();++i){
+        json point = cmd["markers_screen"][i];
+        cout << point["id"] << " " << point["x"] << " " << point["y"] << endl;
+    }
+    cout << "markers_floor" << endl;
+    for (int i = 0;i < cmd["markers_floor"].size();++i){
+        json point = cmd["markers_floor"][i];
+        cout << point["id"] << " " << point["x"] << " " << point["y"] << endl;
+    }
+    cout << "targets_screen" << endl;
+    for (int i = 0;i < cmd["targets_screen"].size();++i){
+        json point = cmd["targets_screen"][i];
+        cout << point["id"] << " " << point["x"] << " " << point["y"] << endl;
+    }
+    cout << "robot_screen" << endl;
+    for (int i = 0;i < cmd["robot_screen"].size();++i){
+        json point = cmd["robot_screen"][i];
+        cout << point["id"] << " " << point["x"] << " " << point["y"] << endl;
+    }
+}
+
+tuple<int, int, int> cmdFwd(const json &cmd){
+    return make_tuple(1, int(stod(string(cmd["val"]))*1000), 0);
+}
+
+tuple<int, int, int> cmdBck(const json &cmd){
+    return make_tuple(2, int(stod(string(cmd["val"]))*1000), 0);
+}
+
+tuple<int, int, int> cmdLft(const json &cmd){
+    return make_tuple(3, int(stod(string(cmd["val"]))*1000), 0);
+}
+
+tuple<int, int, int> cmdRht(const json &cmd){
+    return make_tuple(4, int(stod(string(cmd["val"]))*1000), 0);
+}
+
+tuple<int, int, int> cmdStp(const json &cmd){
+    return make_tuple(0, 0, 0);
+}
+
+tuple<int, int, int> parseJsonCommand(const string &StrCommand){
+    json cmd = json::parse(StrCommand);
+    if (cmd["cmd"] == "auto")
+        return cmdAuto(cmd);
+    else if (cmd["cmd"] == "forward")
+        return cmdFwd(cmd);
+    else if (cmd["cmd"] == "back")
+        return cmdBck(cmd);
+    else if (cmd["cmd"] == "left")
+        return cmdLft(cmd);
+    else if (cmd["cmd"] == "right")
+        return cmdRht(cmd);
+    else if (cmd["cmd"] == "stop")
+        return cmdStp(cmd);
+}
+
+//----------------------------------------------------------------------------------------------------
+
 Subscriber::Subscriber(const char *id, const char *host, int port)
-    : alphabotpp(id)
+    : mosquittopp(id)
 {
     std::cout << app::AppTag << "Connecting to MQTT Broker with address "
               << host << " and port " << port << std::endl;
@@ -33,24 +103,29 @@ void Subscriber::on_connect(int rc)
     }
 }
 
-void Subscriber::on_message(const struct alphabot_message *message)
+
+void Subscriber::on_message(const struct mosquitto_message *message)
 {
     if (Topic == message->topic)
     {
-        const std::string_view payload{reinterpret_cast<char*>(message->payload),
-                                       static_cast<size_t>(message->payloadlen)};
-        NkKosTransport transport;
-        struct alphabot_Printnum_proxy proxy;
+	const std::string tmp_string(static_cast<const char*>(message->payload));
+	NkKosTransport transport;
+        struct mosquitto_Printnum_proxy proxy;
         Handle handle = ServiceLocatorConnect("showapp_connection");
         NkKosTransport_Init(&transport, handle, NK_NULL, 0);
-        nk_iid_t riid = ServiceLocatorGetRiid(handle, "alphabot.Printnum.printnum");
+        nk_iid_t riid = ServiceLocatorGetRiid(handle, "mosquitto.Printnum.printnum");
 
-        alphabot_Printnum_proxy_init(&proxy, &transport.base, riid);
+        mosquitto_Printnum_proxy_init(&proxy, &transport.base, riid);
 
-        alphabot_Printnum_Printnum_req req;
-        alphabot_Printnum_Printnum_res res;
-        req.value = atoi(payload.data()); 
-        alphabot_Printnum_Printnum(&proxy.base, &req, NULL, &res, NULL);
+        mosquitto_Printnum_Printnum_req req;
+        mosquitto_Printnum_Printnum_res res;
+
+	tuple <int, int, int> answer = parseJsonCommand(tmp_string);
+	req.value1 = get<0>(answer);
+	req.value2 = get<1>(answer);
+	req.value3 = get<2>(answer);
+
+	mosquitto_Printnum_Printnum(&proxy.base, &req, NULL, &res, NULL);
     }
 }
 
